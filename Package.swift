6ff82d7244c8da4defe9ab6e7eaabe5d1c5f44ba
Flatout73 <fantom2259@gmail.com>
2021-08-28 00:27:41 +0300

    Fix package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "UseDesk", targets: ["UseDesk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift", from: "16.0.0"),
+        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", from: "16.0.0"),
         .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         .package(url: "https://github.com/sendyhalim/Swime", from: "3.0.0"),
         .package(url: "https://github.com/larryonoff/Down", .branch("fix-imports"))
