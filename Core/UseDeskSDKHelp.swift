565fc52a9b04ab8b84481daa9740c6c629bc3219
Сергей Кононов <kon.sergius@gmail.com>
2023-08-04 11:43:19 +0400

    update for 3.4.6

--- Core/UseDeskSDKHelp.swift
+++ Core/UseDeskSDKHelp.swift
@@ -10,7 +10,7 @@ class UseDeskSDKHelp {
         let payload: [String : Any] = [
             "sdk" : "iOS",
             "type" : "sdk",
-            "version" : "3.4.5",
+            "version" : "3.4.6",
             "message_limit" : countMessagesOnInit,
             "userData" : getUserParameters()
         ] 
