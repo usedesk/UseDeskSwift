7d3bb532c4c42888a38d4d4173662c594e6f0b7b
Сергей Кононов <kon.sergius@gmail.com>
2024-10-03 18:59:10 +0400

    update for 3.4.14

--- Core/UseDeskSDKHelp.swift
+++ Core/UseDeskSDKHelp.swift
@@ -10,7 +10,7 @@ class UseDeskSDKHelp {
         let payload: [String : Any] = [
             "sdk" : "iOS",
             "type" : "sdk",
-            "version" : "3.4.13",
+            "version" : "3.4.14",
             "message_limit" : countMessagesOnInit,
             "userData" : getUserParameters()
         ] 
--- UseDesk_SDK_Swift.podspec
+++ UseDesk_SDK_Swift.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
 	s.name             = 'UseDesk_SDK_Swift'
-	s.version          = '3.4.13'
+	s.version          = '3.4.14'
 	s.summary          = 'A short description of UseDesk.'
 
 	s.homepage         = 'https://github.com/usedesk/UseDeskSwift'
